eee070bc17fadd2b7bd3adb34ed90de2ac4559e8
Muhammad zulqarnain <zulqarnainniazi447@gmail.com>
2023-06-01 12:07:29 -0700

    added dynamic routing using url parsing

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const replaceTemplate = (temp, product) => {
   output = output.replace(/{%FROM%}/g, product.from);
   output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
   output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
   output = output.replace(/{%DESCRIPTION%}/g, product.description);
   output = output.replace(/{%ID%}/g, product.id);
   output = output.replace(/{%DESCRIPTION%}/g, product.description);
@@ -34,18 +35,20 @@ const replaceTemplate = (temp, product) => {
 };
 
 const server = http.createServer((req, res) => {
-  const pathname = req.url;
+  const { pathname, query } = url.parse(req.url, true);
   if (pathname == "/" || pathname == "/overview") {
-    if (pathname == "/") {
-      res.end("Home Page here");
-    } else {
-      res.writeHead(200, { "Content-type": "text/html" });
-      const cardsHtml = dataObj
-        .map((el) => replaceTemplate(tempCard, el))
-        .join("");
-      const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
-      res.end(output);
-    }
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+    res.end(output);
+  } else if (pathname == "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+    console.log(query);
   } else res.writeHead(404);
   res.end("Page Not Found");
 });
